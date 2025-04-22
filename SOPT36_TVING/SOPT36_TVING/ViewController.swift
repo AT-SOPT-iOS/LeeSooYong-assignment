683359dcd2c6bfc3dc1a6130097cc1a059bbbea0
pedro0527 <sooyong0527@naver.com>
2025-04-23 00:13:41 +0900

    [setting] 프로젝트 세팅

---
+++ SOPT36_TVING/SOPT36_TVING/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SOPT36_TVING
+//
+//  Created by LEESOOYONG on 4/23/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
