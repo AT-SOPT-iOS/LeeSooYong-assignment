94eb68a2f959e6df867fc0b82668bea072e5fe7c
pedro0527 <sooyong0527@naver.com>
2025-05-08 21:54:10 +0900

    [fix] 오타 수정

--- SOPT36_TVING/SOPT36_TVING/Extension/Bundle.swift
+++ SOPT36_TVING/SOPT36_TVING/Extension/Bundle.swift
@@ -8,15 +8,15 @@
 import Foundation
 
 extension Bundle {
-    var openWeatherAPIKey: String {
+    var openMovieAPIKey: String {
         get {
             guard let filePath = Bundle.main.path(forResource: "SecretKey", ofType: "plist") else {
                 fatalError("Couldn't find file 'SecretKey.plist'.")
             }
             let plist = NSDictionary(contentsOfFile: filePath)
             
-            guard let value = plist?.object(forKey: "OPENWEATHERMAP_KEY") as? String else {
-                fatalError("Couldn't find key 'OPENWEATHERMAP_KEY' in 'SecretKey.plist'.")
+            guard let value = plist?.object(forKey: "OPENMOVIEAPI_KEY") as? String else {
+                fatalError("Couldn't find key 'OPENMOVIEAPI_KEY' in 'SecretKey.plist'.")
             }
             return value
         }
